1ee3d8dbfd1c5b96f19da660d78cb3e978ba99f5
deepakkumarbind143 <deepakkumarbind949@gmail.com>
2026-03-06 13:35:02 +0530

    concurrency

---
+++ concurrency.dart
@@ -0,0 +1,10 @@
+// Concurrency 
+// Concurrency = multiple tasks making progress at the same time.
+// But not necessarily running at the exact same moment.
+
+// think like this 
+// You cook food
+// You check your phone
+// You stir the pan
+// You reply to message
+
