0ce1566395c034a99d1ca851686e2aa978aa1e35
deepakkumarbind143 <deepakkumarbind949@gmail.com>
2026-03-14 13:48:09 +0530

    functional programing

---
+++ fuctional_programing.dart
@@ -0,0 +1,16 @@
+// We’ll cover the core FP concepts:
+
+// 1️⃣ First-Class Functions
+// 2️⃣ Pure Functions
+// 3️⃣ Immutability
+// 4️⃣ Higher-Order Functions
+// 5️⃣ Anonymous Functions (Lambdas)
+// 6️⃣ Function Composition
+// 7️⃣ Map / Where / Reduce / Fold
+
+
+// 1️⃣ First-Class Functions   //In Dart, functions are values.
+// that means you can 
+// store them in variables
+// pass them as parameters
+// return them from functions
